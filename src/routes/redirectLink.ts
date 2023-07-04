b4e7efc277ecaefd68ffad49a3d31ebfe67d739b
Adebero Oluwagbemisola <adeberogbemisola77@gmail.com>
2023-07-04 07:14:25 -0700

    redirecting the short url

--- src/routes/redirectLink.ts
+++ src/routes/redirectLink.ts
@@ -2,23 +2,35 @@ import express, {Request, Response} from "express";
 import {Link, ILinks} from "../model/linkSchema";
 import "dotenv/config";
 const redirectRoute = express.Router();
+const token: string = process.env.SECRET_KEY as string;
 
 
 
 redirectRoute.get("/:linkId", async(req: Request, res: Response) => {
 
     try {
+
+        //const token = req.cookies.jwt;
         
+
         const getUrl = await Link.findOne({ urlId: req.params.linkId });
         if (getUrl) {
             await getUrl.updateOne({ urlId: req.params.linkId }, {$inc: { clicks: 1 }});
+            const expiryDate = 3 * 24 * 60* 60 * 1000;
+            res.cookie("jwt", token, {
+                httpOnly: true,
+                maxAge: expiryDate,
+                sameSite: "none",
+                secure: true
+            });
             return res.redirect(getUrl.longLink);
+           
         } else {
             res.status(404).json("Url not found!");
         }
 
     } catch (error) {
-        //console.log(error);
+        console.log(error);
         res.status(500).json("Server error");
     }
     
